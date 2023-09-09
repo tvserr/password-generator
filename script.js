35e9175e93f51b321b5e63faf3c355169507ac5a
Joel Serrano <theavserrano@gmail.com>
2023-09-08 18:36:46 -0600

    added prompt for char length

--- script.js
+++ script.js
@@ -2,14 +2,24 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+var characters = ["abcdefghijklmnopqrstuvwxyz0123456789"];
+
 function writePassword() {
-  var characters = "abcdefghijklmnopqrstuvwxyz0123456789"
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  var password = window.prompt("What is the length of your password? Choose from 8 to 128 characters.");
+
+  var index = Math.floor(Math.random() * characters.length);
+  var password = characters[index];
+
+
 }
+
+writePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
