585b1a375aec531d0babd0b985ee9ae365db881f
Joel Serrano <theavserrano@gmail.com>
2023-09-10 01:02:09 -0600

    added prompt for character length

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // character options to pick from in generating random password
+var charLength = [];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -9,7 +10,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
-  
+  var userChoice = window.prompt("How many characters do you want for your password?");
+  if (charLength < 8 || charLength > 128) {
+    window.alert("Please provide a value from 8 to 128.");
+  }
 }
 
 function writePassword() {
