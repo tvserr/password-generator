fe065fd83903cb696090d3976e628b79ed7dac90
Joel Serrano <theavserrano@gmail.com>
2023-09-11 15:05:00 -0600

    added into readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# password-generator
+# Password Generator
+
+## Description
+
+This password generator was created to generate random passwords based on the user's preferences. They may select lowercase or uppercase letters, numbers, or special characters to be included in their password.
+
+## Installation
+
+N/A
+
+## Usage
+
+The following image displays the password generator:
+
+![Password Generator](pg%20screenshot.png)
+
+You will be asked several messages to select your preferences for a random password.
+
+You need to input a number from 8 to 128 which was dictate how many characters you want your password to contain. Next, you will be asked if you want it to contain lowercase letters, uppercase letters, numbers, and/or special characters. This will then generate a random set of characters which will be displayed in the box.
+
+Note: It will not accept a number less than 8 or more than 128 and will give you an alert message telling you to input a valid number.
+
+Try it out here: 
+
+## Credits
+N/A
+
+## License
+None
--- script.js
+++ script.js
@@ -1,6 +1,60 @@
 // character options to pick from for generating a random password
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "@", "#", "$", "%", "&", "?", "/"];
 
@@ -12,25 +66,37 @@ var userChoices = [];
 
 // series of prompts for user to dictate their preference for a random password
 function generatePassword() {
-  var charLength = parseInt(window.prompt("How many characters do you want for your password?"));
+  var charLength = parseInt(
+    window.prompt("How many characters do you want for your password?")
+  );
   while (charLength < 8 || charLength > 128 || isNaN(charLength)) {
     window.alert("Please provide a value from 8 to 128.");
-    charLength = parseInt(window.prompt("How many characters do you want for your password?"));
+    charLength = parseInt(
+      window.prompt("How many characters do you want for your password?")
+    );
   }
-  
-  var uppercaseAns = window.confirm("Do you want to include uppercase letters in your password?");
+
+  var uppercaseAns = window.confirm(
+    "Do you want to include uppercase letters in your password?"
+  );
   if (uppercaseAns) {
     userChoices = userChoices.concat(uppercase);
   }
-  var lowercaseAns =  window.confirm("Do you want to include lowercase letters in your password?");
+  var lowercaseAns = window.confirm(
+    "Do you want to include lowercase letters in your password?"
+  );
   if (lowercaseAns) {
     userChoices = userChoices.concat(lowercase);
   }
-  var numericAns = window.confirm("Do you want to include numbers in your password?");
+  var numericAns = window.confirm(
+    "Do you want to include numbers in your password?"
+  );
   if (numericAns) {
     userChoices = userChoices.concat(numeric);
   }
-  var specialCharAns = window.confirm("Do you want to include special characters in your password?");
+  var specialCharAns = window.confirm(
+    "Do you want to include special characters in your password?"
+  );
   if (specialCharAns) {
     userChoices = userChoices.concat(specialChar);
   }
